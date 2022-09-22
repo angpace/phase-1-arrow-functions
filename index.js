bc1881a627452393f6bd9f1f228e8828c03686c1
Angela Pace <angpace13@gmail.com>
2022-09-21 20:06:13 -0400

    commit tree 8332cab741dcfdafdf21d8feee60150788b23932

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(){
+   return 2000 / 100
+}
+
+const square = (parameter) => {
+    return parameter * parameter
+}
+
+const add = (paramOne, paramTwo) => {
+    return paramOne + paramTwo
+}
